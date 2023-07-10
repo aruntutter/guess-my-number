5868e93c27bbafc321b0a39583dc6a87ce30d98c
Arun Kumar R <aruntutter19@gmail.com>
2023-07-10 18:00:26 +0530

    js- logic done

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,49 @@
 "use strict";
+
+const guessEl = document.getElementById("user-input");
+const btnGuess = document.getElementById("btn-guess");
+const resultEl = document.getElementById("result");
+const guessesEl = document.getElementById("guesses");
+
+// Global variable
+let guesses;
+let randomNumber;
+
+// Functions
+
+const init = () => {//this fun. is having all initial settings 
+  guesses = 0;
+  randomNumber = getRandomNumber();
+};
+
+const getRandomNumber = () => {
+  const randomNumber = Math.trunc(Math.random() * 100) + 1;
+  // if *100); it will generate 0,1,2...99
+  //if *100)+1; it will generate 1,2,...100
+  return randomNumber;
+};
+
+// event listener
+btnGuess.addEventListener("click", () => {
+  const userGuess = Number(guessEl.value);
+
+  if (userGuess) {
+    if (userGuess > randomNumber) {
+      guesses += 1;
+      resultEl.innerText = `Your Guess is high`;
+    } else if (userGuess < randomNumber) {
+      guesses += 1;
+      resultEl.innerText = `Your Guess is low`;
+    } else if (userGuess === randomNumber) {
+      guesses += 1;
+      resultEl.innerText = `Your Guess is Correct \nand the number is ${randomNumber}!`;
+      guessesEl.innerText = `You have guessed in '${guesses}' chances`;
+    }
+  } else {
+    // validation
+    alert(`You should guess a number`);
+  }
+});
+
+// initial setup
+init();
